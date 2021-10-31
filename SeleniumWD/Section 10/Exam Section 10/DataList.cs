1d57e01d5ea45cc5a6774a9aeeec662a81473362
Herbert Quijano <herbert.quijano@gmail.com>
2021-10-30 21:51:32 -0500

    Section 10

---
+++ SeleniumWD/Section 10/Exam Section 10/DataList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWD.Section_10.Exam_Section_10
+{
+    class DataList
+    {
+        public static ArrayList SortData(ArrayList data)
+        {
+            int intValue;
+            string stringData;
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                stringData = data[i].ToString();
+                if (int.TryParse(stringData, out intValue) == false)
+                {
+                    data.Remove(data[i]);
+                }
+                else
+                    int.Parse(data[i].ToString());
+                
+                 
+            }
+            return data;
+        }
+
+        public static double TotalNums(ArrayList data)
+        {
+            double value;
+            double sum = 0;
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                value = double.Parse(data[i].ToString());
+                sum += value;
+            }
+
+            return sum;
+        }
+
+
+    }
+}
---
+++ SeleniumWD/Section 10/Exam Section 10/DataListTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+
+namespace SeleniumWD.Section_10.Exam_Section_10
+{
+    [TestClass]
+    public class DataListTest
+    {
+        ArrayList dataList = new ArrayList();
+        
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ArrayList sortedList = new ArrayList();
+            double total = 0;
+
+            dataList.Add("Someday");
+            dataList.Add(2);
+            dataList.Add("app");
+            dataList.Add(13.5);
+            dataList.Add("red");
+            dataList.Add(8);
+            dataList.Add(99);
+            dataList.Add("6");
+            dataList.Add("Meatball Soup");
+            dataList.Add(18M);
+            dataList.Add("pieces");
+            dataList.Add("14");
+
+            sortedList = DataList.SortData(dataList);
+            total = DataList.TotalNums(sortedList);
+
+            //Console.WriteLine(total);
+            Assert.AreEqual(147, total, 0.1);
+
+               
+            
+
+
+
+
+
+
+        }
+    }
+}
